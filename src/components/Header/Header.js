d5f40fea084d17a8c340d5b2aaf5d3086a1d0e2e
JunayetTamim <junayettamim186@gmail.com>
2022-04-10 00:29:05 +0600

    install and implement react hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../Hooks/useFirebase';
+import app from '../../firebase.init';
+
 import './Header.css'; 
 
+const auth = getAuth(app);
+
 const Header = () => {
-    const { user, signOutFromGoogle}=useFirebase();
+   // const { user, signOutFromGoogle}=useFirebase();
+    const [user, ] = useAuthState(auth);
     return (
         <div className='header'>
             <nav>
@@ -14,7 +20,7 @@ const Header = () => {
                 <Link to='/register'>Register</Link>
                 
                 { user?.uid ?
-                    <button onClick={signOutFromGoogle}>Sign out</button> :
+                    <button onClick={()=> signOut(auth)}>Sign out</button> :
                     <Link to='/login'>Log In</Link> 
                     
                 }
--- src/components/LogIn/Login.js
+++ src/components/LogIn/Login.js
@@ -1,13 +1,18 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../Hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
 
 const Login = () => {
-    const { signInWithGoogle} = useFirebase();
+   // const { signInWithGoogle} = useFirebase();
+   const [signInWithGoogle] = useSignInWithGoogle(auth);
     return (
         <div>
             <h2>Please Log In</h2>
             <div style={{margin: '20px'}}>
-                <button onClick={signInWithGoogle}>Google SignIn</button>
+                <button onClick={()=> signInWithGoogle()}>Google SignIn</button>
             </div>
             <form>
                 
